34c39f488bb7705301c1d015f67c90bfd9fe0a8f
marcosm <marcos.martilotta@gmail.com>
2023-03-24 18:02:44 -0300

    Background con movimiento

--- src/sections/projects/components/ProjectCard.jsx
+++ src/sections/projects/components/ProjectCard.jsx
@@ -1,4 +1,6 @@
 import { Card } from "../styled-components/projectCard.styled";
+import trapeze from "../assets/trapeze.png";
+import triangle from "../assets/triangle.png";
 
 const ProjectCard = ({ number, projectName }) => {
   return (
@@ -7,6 +9,13 @@ const ProjectCard = ({ number, projectName }) => {
       <div className="projectName">{projectName}</div>
       <div className="topTrapeze"></div>
       <div className="bottomTrapeze"></div>
+      <div className="background">
+        <img src={triangle} alt="background triangle of the card" />
+        <img src={trapeze} alt="background trapeze of the card" />
+      </div>
+      <div className="linksContainer">
+        <a href="">a ver</a>
+      </div>
     </Card>
   );
 };
--- src/sections/projects/styled-components/projectCard.styled.js
+++ src/sections/projects/styled-components/projectCard.styled.js
@@ -5,9 +5,40 @@ export const Card = styled.div`
   margin: 2rem;
   width: 16rem;
   height: 16rem;
-  background-color: white;
   transition: 0.5s;
   cursor: pointer;
+
+  .background {
+    width: 100%;
+    img {
+      width: 100%;
+    }
+
+    img:nth-child(1) {
+      transform: translateY(0rem);
+      transition: transform 1s ease-out;
+    }
+
+    img:nth-child(2) {
+      position: absolute;
+      top: -0.1rem;
+      z-index: -1;
+      transform: translateY(0rem);
+      transition: transform 1s ease-out;
+    }
+  }
+
+  .background:hover {
+    img:nth-child(1) {
+      transform: translateY(-16rem);
+      transition: transform 1s ease-in;
+    }
+
+    img:nth-child(2) {
+      transform: translateY(20rem);
+      transition: transform 1s ease-in;
+    }
+  }
 
   @media (min-width: 576px) {
     width: 16rem;
@@ -22,13 +53,13 @@ export const Card = styled.div`
   @media (min-width: 992px) {
     width: 24rem;
     height: 24rem;
-    &:hover {
-      transform: translateY(10px);
-    }
   }
 
   @media (min-width: 1200px) {
     width: 24rem;
+  }
+
+  .linksContainer {
   }
 
   .number {
@@ -36,6 +67,7 @@ export const Card = styled.div`
     top: 0rem;
     left: 1rem;
     font-size: 6rem;
+    z-index: 2;
   }
 
   .projectName {
@@ -45,5 +77,8 @@ export const Card = styled.div`
     font-size: 2rem;
     width: 8rem;
     text-align: right;
+  }
+
+  .background {
   }
 `;
