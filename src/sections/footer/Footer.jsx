3c489d9f363ea9aca80e1d7e51a7fb3901211a56
marcosm <marcos.martilotta@gmail.com>
2023-04-19 21:11:50 -0300

    cambio en cv

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -1,33 +1,33 @@
 import { StyledFooter } from "./styled-components/footer.styled";
 
 const Footer = () => {
-  return (
-    <StyledFooter>
-      <ul>
-        <li>
-          <a
-            href="https://drive.google.com/file/d/1wpO08MoRCxC-jhLprqFFZpAli4vLYZsD/view?usp=sharing"
-            target="_blank"
-          >
-            Download my CV
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.linkedin.com/in/marcos-octavio-martilotta-450a621a6/"
-            target="_blank"
-          >
-            LinkedIn
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/MarcosMartilotta" target="_blank">
-            GitHub
-          </a>
-        </li>
-      </ul>
-    </StyledFooter>
-  );
+	return (
+		<StyledFooter>
+			<ul>
+				<li>
+					<a
+						href="https://drive.google.com/file/d/1yFYDAzgPD0NOrY5nNpV3uea_jMDkXOnd/view?usp=share_link"
+						target="_blank">
+						Download my CV
+					</a>
+				</li>
+				<li>
+					<a
+						href="https://www.linkedin.com/in/marcos-octavio-martilotta-450a621a6/"
+						target="_blank">
+						LinkedIn
+					</a>
+				</li>
+				<li>
+					<a
+						href="https://github.com/MarcosMartilotta"
+						target="_blank">
+						GitHub
+					</a>
+				</li>
+			</ul>
+		</StyledFooter>
+	);
 };
 
 export default Footer;
