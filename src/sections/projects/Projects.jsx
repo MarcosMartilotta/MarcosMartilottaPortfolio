3e62e2efb60c4283b6894f945d3107eb8fe75d65
marcosm <marcos.martilotta@gmail.com>
2023-06-15 08:54:43 -0300

    Farmaturno a proyectos

--- src/sections/projects/Projects.jsx
+++ src/sections/projects/Projects.jsx
@@ -8,98 +8,99 @@ import climApp from "./assets/climApp.jpeg";
 import fairPrice from "./assets/fairPrice.jpeg";
 import mp4Player from "./assets/reproductorVideos.jpg";
 import todoList from "./assets/ToDoList.jpg";
+import farmaturno from "./assets/farmaturno.jpeg";
 
 const Projects = () => {
-  const settings = {
-    centerMode: true,
-    focusOnSelect: true,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 1000,
-    centerPadding: "30px",
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    speed: 2000,
-    pauseOnHover: true,
-    swipeToSlide: true,
-    swipe: true,
-    touchMove: true,
-    arrows: false,
+	const settings = {
+		centerMode: true,
+		focusOnSelect: true,
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 1000,
+		centerPadding: "30px",
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		speed: 2000,
+		pauseOnHover: true,
+		swipeToSlide: true,
+		swipe: true,
+		touchMove: true,
+		arrows: false,
 
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          centerMode: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          initialSlide: 0,
-          autoplay: false,
-          speed: 500,
-          arrows: false,
-          centerMode: false,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          autoplay: false,
-          speed: 500,
-          centerMode: false,
-        },
-      },
-    ],
-  };
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					infinite: true,
+					centerMode: true,
+				},
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					initialSlide: 0,
+					autoplay: false,
+					speed: 500,
+					arrows: false,
+					centerMode: false,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					autoplay: false,
+					speed: 500,
+					centerMode: false,
+				},
+			},
+		],
+	};
 
-  return (
-    <StyledProjects>
-      <div className="titleContainer">
-        <SectionTitle title="Projects" />
-      </div>
-      <div className="projectsContainer">
-        <Slider {...settings}>
-          <ProjectCard
-            number="1"
-            projectName="El valor justo"
-            pageLink="https://deluxe-lollipop-24649a.netlify.app/"
-            repoLink="https://github.com/MarcosMartilotta/fairPrice"
-            preview={fairPrice}
-          />
-          <ProjectCard
-            number="2"
-            projectName="ClimApp"
-            pageLink="https://marcosmartilotta.github.io/climApp/"
-            repoLink="https://github.com/MarcosMartilotta/climApp"
-            preview={climApp}
-          />
-          <ProjectCard
-            number="3"
-            projectName="Mp4 Player"
-            pageLink="https://marcosmartilotta.github.io/reproductor_mp4/"
-            repoLink="https://github.com/MarcosMartilotta/reproductor_mp4"
-            preview={mp4Player}
-          />
-          <ProjectCard
-            number="4"
-            projectName="ToDoList"
-            pageLink="https://marcosmartilotta.github.io/ToDoList/"
-            repoLink="https://github.com/MarcosMartilotta/ToDoList"
-            preview={todoList}
-          />
-        </Slider>
-      </div>
-    </StyledProjects>
-  );
+	return (
+		<StyledProjects>
+			<div className="titleContainer">
+				<SectionTitle title="Projects" />
+			</div>
+			<div className="projectsContainer">
+				<Slider {...settings}>
+					<ProjectCard
+						number="1"
+						projectName="FarmaTurno"
+						pageLink="https://c11-02-m-mern-farmaturno.vercel.app/"
+						repoLink="https://github.com/No-Country/c11-02-m-mern-farmaturno"
+						preview={farmaturno}
+					/>
+					<ProjectCard
+						number="2"
+						projectName="ClimApp"
+						pageLink="https://marcosmartilotta.github.io/climApp/"
+						repoLink="https://github.com/MarcosMartilotta/climApp"
+						preview={climApp}
+					/>
+					<ProjectCard
+						number="3"
+						projectName="Mp4 Player"
+						pageLink="https://marcosmartilotta.github.io/reproductor_mp4/"
+						repoLink="https://github.com/MarcosMartilotta/reproductor_mp4"
+						preview={mp4Player}
+					/>
+					<ProjectCard
+						number="4"
+						projectName="ToDoList"
+						pageLink="https://marcosmartilotta.github.io/ToDoList/"
+						repoLink="https://github.com/MarcosMartilotta/ToDoList"
+						preview={todoList}
+					/>
+				</Slider>
+			</div>
+		</StyledProjects>
+	);
 };
 
 export default Projects;
